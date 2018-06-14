1b8d7c40b732cddaaf22eab100a2eb70876e4263
AZimmermann89 <Zimmermann.Alexander89@gmail.com>
2018-06-14 12:44:57 +0200

    Change gitignore and add a comment where I am currently

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <code/RenderCore.h>
 #include <iostream>
 
+// https://vulkan-tutorial.com/Texture_mapping/Image_view_and_sampler somewhere here I guess?
+
 int main() {
     EngineCore::RenderCore rendCore{};
     
